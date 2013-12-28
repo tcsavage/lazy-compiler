b053baf9a12ee038e8cce5ac05046b0cc1c5a118
tcsavage <tcsavage@gmail.com>
2013-12-28 22:41:33 +0000

    Switched over to using StateT for GCode interpreter. Started implementing Alloc instruction.

--- src/GMachine/Interpreter.hs
+++ src/GMachine/Interpreter.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE PackageImports #-}
 {- Implements a G-Machine interpreter as outlined in "Implementing Functional Languages a tutorial" Peyton Jones, Lester (2000). -}
 
 module GMachine.Interpreter where
 
 import Control.Applicative
 import Control.Monad
+import "mtl" Control.Monad.State
 import qualified Data.Map as M
 import Data.Maybe
 import Data.Unique
+import qualified System.IO.Unsafe
 import Text.Printf
 import Text.Show.Pretty (ppShow)
 
@@ -37,6 +40,22 @@ data Node = NNum Int  -- Value
 instance Show Unique where
     show = show . hashUnique
 
+-- | State computation over GMState in IO monad.
+type GM a = StateT GMState IO a
+
+runGM :: GM a -> GMState -> IO (a, GMState)
+runGM = runStateT
+
+evalGM :: GM a -> GMState -> IO a
+evalGM = evalStateT
+
+execGM :: GM a -> GMState -> IO GMState
+execGM = execStateT
+
+-- Grap a heap address to represent NULL. Doesn't matter what it is.
+heapNull :: Addr
+heapNull = System.IO.Unsafe.unsafePerformIO newUnique
+
 mkState :: [(Name, GMCode, Int)] -> IO GMState
 mkState globals = do
     (globalTable, heap) <- initGlobals globals
@@ -60,29 +79,43 @@ initGlobal (name, code, arity) = do
 stateFinal :: GMState -> Bool
 stateFinal = null . gmInst
 
+isFinal :: GM Bool
+isFinal = fmap stateFinal get
+
+setCode :: [Instruction Name] -> GM ()
+setCode code = do
+    s0 <- get
+    put $ s0 { gmInst = code }
+
 -- | Push an address onto the stack.
-stackPush :: Addr -> GMState -> GMState
-stackPush addr s0 = s0 { gmStack = addr:stack0 }
-    where
-        stack0 = gmStack s0
+stackPush :: Addr -> GM ()
+stackPush addr = do
+    s0 <- get
+    let stack0 = gmStack s0
+    put $ s0 { gmStack = addr:stack0 }
 
 -- | Pop.
-stackPop :: GMState -> (Addr, GMState)
-stackPop s0 = (head stack0, s0 { gmStack = drop 1 stack0 })
-    where
-        stack0 = gmStack s0
+stackPop :: GM Addr
+stackPop = do
+    s0 <- get
+    let (a:as) = gmStack s0
+    put $ s0 { gmStack = as }
+    pure a
 
 -- | Pop many.
-stackPopMany :: Int -> GMState -> GMState
-stackPopMany n s0 = s0 { gmStack = drop n stack0 }
-    where
-        stack0 = gmStack s0
+stackPopMany :: Int -> GM ()
+stackPopMany n = do
+    s0 <- get
+    let stack0 = gmStack s0
+    put $ s0 { gmStack = drop n stack0 }
 
 -- | Peek.
-stackPeek :: GMState -> Addr
-stackPeek s0 = top
-    where
-        (top:_) = gmStack s0
+stackPeek :: GM Addr
+stackPeek = (head . gmStack) <$> get
+
+-- | Get the current number of stack items.
+stackSize :: GM Int
+stackSize = fmap (length . gmStack) get
 
 -- | Apply a function to the head of a list only.
 appHead :: (a -> a) -> [a] -> [a]
@@ -90,26 +123,28 @@ appHead _ [] = []
 appHead f (x:xs) = f x : xs
 
 -- | Stack rewrite.
-stackRewrite :: Addr -> Int -> GMState -> GMState
-stackRewrite addr pos s0 = s0 { gmStack = top ++ appHead (const addr) btm }
-    where
-      (top, btm) = splitAt pos $ gmStack s0
+stackRewrite :: Addr -> Int -> GM ()
+stackRewrite addr pos = do
+    s0 <- get
+    let (top, btm) = splitAt pos $ gmStack s0
+    put $ s0 { gmStack = top ++ appHead (const addr) btm }
 
 -- | Return the heap address of a global.
-getGlobal :: Name -> GMState -> Addr
-getGlobal name = fromMaybe (error $ printf "Couldn't find global `%s`" name) . M.lookup name . gmGlobals
+getGlobal :: Name -> GM Addr
+getGlobal name = (fromMaybe (error $ printf "Couldn't find global `%s`" name) . M.lookup name . gmGlobals) <$> get
 
 -- | "Allocate" a node on the "heap".
-hAlloc :: Node -> GMState -> IO (Addr, GMState)
-hAlloc node s0 = do
-    addr <- newUnique
-    pure (addr, s0 { gmHeap = M.insert addr node heap0 })
-  where
-    heap0 = gmHeap s0
+hAlloc :: Node -> GM Addr
+hAlloc node = do
+    addr <- liftIO newUnique
+    s0 <- get
+    let heap0 = gmHeap s0
+    put $ s0 { gmHeap = M.insert addr node heap0 }
+    pure addr
 
 -- | Lookup an node on the "heap" by its address.
-hDeref :: Addr -> GMState -> Node
-hDeref addr = fromMaybe (error "Failed to dereference heap address.") . M.lookup addr . gmHeap
+hDeref :: Addr -> GM Node
+hDeref addr = (fromMaybe (error "Failed to dereference heap address.") . M.lookup addr . gmHeap) <$> get
 
 -- | Helper function for getting the address of the nth argument.
 getArg :: Node -> Addr
@@ -142,71 +177,90 @@ evalToValue s0 = do
 
 -- | Step the machine.
 step :: GMState -> IO GMState
-step s0 = dispatch i (s0 { gmInst = is })
+step s0 = execGM (dispatch i) (s0 { gmInst = is })
     where
         (i:is) = gmInst s0
 
 -- | Select the instruction handler function.
-dispatch :: Instruction Name -> GMState -> IO GMState
+dispatch :: Instruction Name -> GM ()
 dispatch (PushGlobal name) = pushGlobal name
 dispatch (PushInt n) = pushInt n
 dispatch (Push x) = push x
 dispatch MkAp = mkAp
 dispatch (Slide n) = slide n
+dispatch (Alloc n) = alloc n
 dispatch (Update n) = update n
 dispatch (Pop n) = pop n
 dispatch Unwind = unwind
 
 -- | Lookup the address of the given global and push it onto the stack.
-pushGlobal :: Name -> GMState -> IO GMState
-pushGlobal name s0 = pure $ stackPush (getGlobal name s0) s0
+pushGlobal :: Name -> GM ()
+pushGlobal name = do
+    g <- getGlobal name
+    stackPush g
 
 -- | Allocate a new integer node on the heap and push its address onto the stack.
-pushInt :: Int -> GMState -> IO GMState
-pushInt n s0 = do
-    (addr, s1) <- hAlloc (NNum n) s0
-    pure $ stackPush addr s1
+pushInt :: Int -> GM ()
+pushInt n = do
+    addr <- hAlloc $ NNum n
+    stackPush addr
 
 -- | Push the address of the nth argument onto the stack.
-push :: Int -> GMState -> IO GMState
-push x s0 = pure $ stackPush (getArg $ hDeref (stack0 !! (x+1)) s0) s0
-    where
-        stack0 = gmStack s0
+push :: Int -> GM ()
+push x = do
+    stack0 <- gmStack <$> get
+    node <- hDeref (stack0 !! (x+1))
+    stackPush $ getArg node
 
 -- | Pop the tow two addresses from the stack. Allocate a new application node on the heap using those two addresses, and push the new node's adderss onto the stack.
-mkAp :: GMState -> IO GMState
-mkAp s0 = do
-    let (x1:x2:xs) = gmStack s0
-    (addr, s1) <- hAlloc (NAp x1 x2) s0
-    pure $ s1 { gmStack = addr:xs }
+mkAp :: GM ()
+mkAp = do
+    x1 <- stackPop
+    x2 <- stackPop
+    addr <- hAlloc (NAp x1 x2)
+    stackPush addr
 
 -- | Pop the top address from the stack and call it @x@. Pop the next @n@ items on the stack and discard them. Push @x@ back onto the stack.
-slide :: Int -> GMState -> IO GMState
-slide n s0 = pure $ s0 { gmStack = x : drop n xs }
+slide :: Int -> GM ()
+slide n = do
+    x <- stackPop
+    stackPopMany n
+    stackPush x
+
+alloc :: Int -> GM ()
+alloc n = undefined
     where
-        (x:xs) = gmStack s0
+        allocNodes :: Int -> GMHeap -> (GMHeap, [Addr])
+        allocNodes 0 heap = (heap, [])
 
-update :: Int -> GMState -> IO GMState
-update 0 s0 = pure s0
-update n s0 = do
-    let (top, s1) = stackPop s0
-    (addr, s2) <- hAlloc (NInd top) s1
-    pure $ stackRewrite addr n s2
+update :: Int -> GM ()
+update 0 = pure ()
+update n = do
+    top <- stackPop
+    addr <- hAlloc $ NInd top
+    stackRewrite addr n
 
-pop :: Int -> GMState -> IO GMState
-pop 0 = pure
-pop n = pure . stackPopMany n
+pop :: Int -> GM ()
+pop 0 = pure ()
+pop n = stackPopMany n
 
 -- | Traverse the graph's application nodes from HEAD and push their addresses onto the stack. Stop at the first global and prepare it to run.
-unwind :: GMState -> IO GMState
-unwind state = pure $ newState $ hDeref a state
+unwind :: GM ()
+unwind = do
+    top <- stackPeek
+    node <- hDeref top
+    newState node
     where
-        (a:as) = gmStack state
-        newState (NNum n) = state
-        newState (NAp f x) = state { gmInst = [Unwind], gmStack = f:a:as }
-        newState (NGlobal n code)
-            | length as < n = error "Unwinding with too few arguments."
-            | otherwise = state { gmInst = code }
-        newState (NInd addr) = state { gmInst = [Unwind], gmStack = addr:as }
-            where
-                (a:as) = gmStack state
+        newState (NNum n) = pure ()
+        newState (NAp f x) = do
+            stackPush f
+            setCode [Unwind]
+        newState (NGlobal n code) = do
+            size <- stackSize
+            case (size - 1) < n of
+                True -> error "Unwinding with too few arguments."
+                False -> setCode code
+        newState (NInd addr) = do
+            stackPop
+            stackPush addr
+            setCode [Unwind]
