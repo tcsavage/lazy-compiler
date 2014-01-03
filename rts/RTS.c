8e7b6bede625f9d999bb7db432ace769ba76ae69
tcsavage <tcsavage@gmail.com>
2014-01-03 21:58:07 +0000

    Move RTS into own directory

--- rts/RTS.c
+++ rts/RTS.c
@@ -41,6 +41,19 @@ void printNode(Node *node);
 
 void doUnwind(Instruction *ins);
 
+void* wrappedMallock(size_t size, const char *file, int line, const char *func)
+{
+
+    void *p = malloc(size);
+    printf ("Allocated = %s, %i, %s, %p[%d]\n", file, line, func, p, size);
+
+    /*Link List functionality goes in here*/
+
+    return p;
+}
+
+#define malloc(X) wrappedMallock( X, __FILE__, __LINE__, __FUNCTION__)
+
 // Create a new, empty stack.
 Stack *mkStack(int size) {
     Stack *stack = (Stack *) malloc(sizeof(Stack));
