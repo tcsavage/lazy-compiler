c210352528c438c784af63a38d70cca1d65ba843
tcsavage <tcsavage@gmail.com>
2014-01-04 16:13:31 +0000

    Only show memory allocation information when compiled with DEBUGMM preprocessor macro

--- cviac.sh
+++ cviac.sh
@@ -2,4 +2,4 @@
 
 ./cabal-dev/bin/simplelang -bviac $1
 
-gcc -I rts $1.c rts/RTS.c rts/Dump.c rts/Env.c rts/Instruction.c rts/Node.c rts/Stack.c rts/Memory.c
+gcc -DDEBUGMM -I rts $1.c rts/RTS.c rts/Dump.c rts/Env.c rts/Instruction.c rts/Node.c rts/Stack.c rts/Memory.c
--- rts/MemoryDefs.h
+++ rts/MemoryDefs.h
@@ -1,7 +1,9 @@
 #ifndef RTS_MEMORY_DEFS_H
 #define RTS_MEMORY_DEFS_H
 
+#ifdef DEBUGMM
 #define malloc(X) wrappedMalloc(X, __FILE__, __LINE__, __FUNCTION__)
 #define free(X) wrappedFree(X, __FILE__, __LINE__, __FUNCTION__)
+#endif
 
 #endif
