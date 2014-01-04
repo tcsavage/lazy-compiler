5dadc851cf4917cbb2f412d7167a3719b79b1235
tcsavage <tcsavage@gmail.com>
2014-01-04 16:13:49 +0000

    More readable memory allocation debug information.

--- rts/Memory.c
+++ rts/Memory.c
@@ -2,12 +2,12 @@
 
 void* wrappedMalloc(size_t size, const char *file, int line, const char *func) {
     void *p = malloc(size);
-    printf ("Allocated = %s, %i, %s, %p[%d]\n", file, line, func, p, size);
+    printf ("Allocated %d bytes (%p) in %s (%s:%i)\n", size, p, func, file, line);
 
     return p;
 }
 
 void wrappedFree(void *p, const char *file, int line, const char *func) {
     free(p);
-    printf ("Freed = %s, %i, %s, %p\n", file, line, func, p);
+    printf ("Freed %p in %s (%s:%i)\n", p, func, file, line);
 }
