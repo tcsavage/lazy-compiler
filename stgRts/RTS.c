435b4b2092bdcf4647e68a28bd5f8eb47508687e
tcsavage <tcsavage@gmail.com>
2014-01-19 00:29:04 +0000

    Been implementing basic RTS functionality and playing with hand-written closures and info tables.

--- stgRts/RTS.c
+++ stgRts/RTS.c
@@ -1,9 +1,172 @@
-#import "RTS.h"
+#include "RTS.h"
+#include "stdlib.h"
+#include "stdio.h"
 
-CodeLabel cont;
+StgFunPtr cont;
 
 void run() {
-    while (TRUE) {
-        cont = (*cont)();
+    while (1) {
+        cont = (StgFunPtr)(cont)();
     }
+}
+
+void initStacks(int size) {
+    size_t realsize = size*sizeof(StgAddr);
+    spALim = spABase = spA = malloc(realsize);
+    spBLim = spBBase = spB = spALim + realsize;
+}
+
+void freeStacks() {
+    free(spALim);
+}
+
+inline StgAddr allocClosure(StgWord *closure) {
+
+}
+
+void initHeap(int size) {
+    size_t realsize = size*sizeof(StgAddr);
+    hpLim = hp = malloc(realsize);
+}
+
+void freeHeap() {
+    free(hpLim);
+}
+
+inline StgAddr popA() {
+    --spA;
+    return spA[0];
+}
+
+inline StgAddr peekA() {
+    return spA[-1];
+}
+
+inline void pushA(StgAddr v) {
+    spA[0] = v;
+    ++spA;
+}
+
+inline StgAddr popB() {
+    ++spB;
+    return spB[0];
+}
+
+inline StgAddr peekB() {
+    return spB[1];
+}
+
+inline void pushB(StgAddr v) {
+    spB[0] = v;
+    --spB;
+}
+
+Value *mkValueInt(StgInt data) {
+    Value *val = (Value *) malloc(sizeof(Value));
+    val->type = VALUE_PINT;
+    val->data.primInt = data;
+    return val;
+}
+
+Value *mkValueAddr(StgAddr data) {
+    Value *val = (Value *) malloc(sizeof(Value));
+    val->type = VALUE_ADDR;
+    val->data.closure = data;
+    return val;
+}
+
+void printValue(Value *val) {
+    switch (val->type) {
+        case VALUE_PINT:
+            printf("Int value: %d\n", val->data.primInt);
+            break;
+        case VALUE_ADDR:
+            printf("Closure value: %p\n", val->data.closure);
+            break;
+        default:
+            printf("Unknown value type.\n");
+    }
+}
+
+StgFunPtr black_hole_entry() {
+    printf("Black hole error.\n");
+    exit(1);
+}
+
+//////
+// Test code
+//////
+
+void dumpInt(StgAddr x) {
+    printf("dumpInt: %d\n", (StgInt)x);
+    exit(0);
+}
+
+StgFunPtr h() {
+    printf("In h\n");
+    exit(0);
+}
+
+StgFunPtr g() {
+    printf("In g\n");
+    JUMP(h);
+}
+
+StgFunPtr f() {
+    printf("In f\n");
+    JUMP(g);
+}
+
+StgFunPtr test_entry() {
+    printf("Test entry code. Closure value 1: %d\n", (StgInt)node[1]);
+    JUMP(f);
+}
+
+StgWord test_info[] = {(StgWord)test_entry};
+
+StgWord test_closure[] = {(StgWord)test_info, 12};
+
+StgFunPtr dumpInt_entry() {
+    dumpInt(popA());
+}
+
+StgWord dumpInt_info[] = {(StgWord)dumpInt_entry};
+
+StgWord dumpInt_closure[] = {(StgWord)dumpInt_info};
+
+StgFunPtr double_entry() {
+    // (U) Argument satisfaction check.
+    // Stack overflow check.
+    // Heap overflow check.
+    // (U) Info pointer update.
+    // (U) Update frame construction.
+
+    // Actual code:
+    StgInt x = (StgInt)popA();
+    pushA((StgAddr)(x*2));
+    ENTER(dumpInt_closure);
+}
+
+StgWord double_info[] = {(StgWord)double_entry};
+
+// double x = x*2
+StgWord double_closure[] = {(StgWord)double_info};
+
+StgFunPtr start() {
+    pushA((StgAddr)678);
+    node = double_closure;
+    ENTER(node);
+}
+
+int main(int argc, char const *argv[])
+{
+    initStacks(256);
+    initHeap(256);
+    Value *val = mkValueInt(54);
+    printValue(val);
+    cont = (StgFunPtr)start;
+    run();
+    freeHeap();
+    freeStacks();
+    return 0;
 }
--- stgRts/RTS.h
+++ stgRts/RTS.h
@@ -4,16 +4,19 @@
 #define UF_UPDATE 1
 #define UF_NOUPDATE 2
 
-#define VALUE_ADDR
-#define VALUE_PINT
+#define VALUE_ADDR 1
+#define VALUE_PINT 2
 
 #define CODE_EVAL 1
 #define CODE_ENTER 2
 #define CODE_RETURNCON 3
 #define CODE_RETURNINT 4
 
-#define JUMP(lbl) return lbl  // Can be changed for platform-specific optimisations.
-#define ENTER(c) JUMP(**c)  // Can be changed for platform-specific optimisations.
+// Jump to a specified code label (function pointer).
+#define JUMP(lbl) return (StgFunPtr)lbl  // Can be changed for platform-specific optimisations.
+
+// Enter the given closure by JUMPing to its standard entry code.
+#define ENTER(c) JUMP(*((StgAddr *) *c))  // Can be changed for platform-specific optimisations.
 
 typedef signed   char            StgInt8;
 typedef unsigned char            StgWord8;
@@ -33,44 +36,80 @@ typedef void*                    StgAddr;
 typedef void  *(*(*StgFunPtr)(void))(void);
 typedef StgFunPtr StgFun(void);
 
-typedef (CodeLabel)(*CodeLabel)();
-
+// Statically-defined table shared for all closure instances.
 typedef struct CInfo
 {
-    CodeLabel standardEntryCode;
-    CodeLabel evacuationCode;
-    CodeLabel scavengeCode;
+    StgFunPtr standardEntryCode;
+    StgFunPtr evacuationCode;
+    StgFunPtr scavengeCode;
     int pointerCount;
     int valueCount;
 } CInfo;
 
+// A "value" can either be a primitive value (like a machine int) or a pointer to a closure allocated on the heap.
 typedef struct Value
 {
     int type;
-    StgAddr data;
+    union {
+        StgAddr closure;
+        StgInt primInt;
+    } data;
 } Value;
 
-typedef struct HeapValue
-{
-    Closure *closure;
-} HeapValue;
+// Allocate and initialize a new machine int value.
+Value *mkValueInt(StgInt data);
 
-typedef struct PrimIntValue
-{
-    Int value;
-} PrimIntValue;
+// Allocate and initialize a new closure value.
+Value *mkValueAddr(StgAddr data);
 
+// Dump a value's type and associated data to the console (for debugging).
+void printValue(Value *val);
+
+// When an updatable closure is entered, it pushes an update frame onto upd, and clears the stacks.
 typedef struct UpdateFrame
 {
-    Stack_Value *argS;  // Old arg stack.
-    Stack_Cont *retS;  // Old ret stack.
-    Closure *closure;
+    StgAddr *sA;  // Old arg stack.
+    StgAddr *sB;  // Old control stack.
+    StgAddr closure;  // The closure to be updated.
 } UpdateFrame;
 
+inline StgAddr popA();
+inline StgAddr peekA();
+inline void pushA(StgAddr);
+
+inline StgAddr popB();
+inline StgAddr peekB();
+inline void pushB(StgAddr);
+
+// Allocate space for the stacks and set the values of the stack pointers to their initial positions.
+void initStacks(int);
+
+// Free stack memory.
+void freeStacks();
+
+inline StgAddr allocClosure(StgWord *);
+
+// Allocate space for a heap and initialize the heap pointer.
+void initHeap(int);
+
+// Free heap memory.
+void freeHeap();
+
+// Back hole.
+StgFunPtr black_hole_entry();
+static StgWord blackHole_info[] = {(StgWord)black_hole_entry};
+
 // Globals.
-Closure *node;
-Stack_Value *argS;
-Stack_Cont *retS;
-Stack_UpdateFrame *updS;
+static StgWord *node;  // Current closure. `node[0]` is the info table (StgWord *).
+static StgAddr *spA;  // Arg stack. Contains values. Grows upwards.
+static StgAddr *spB;  // COntrol stack. Contains return vectors (continuations) and update frames. Grows downwards.
+static StgAddr *spALim;  // Marks bottom of stack space.
+static StgAddr *spBLim;  // Marks top of stack space.
+static StgAddr *spABase;  // Marks bottom of ACTIVE stack space.
+static StgAddr *spBBase;  // Marks top of ACTIVE stack space.
+static StgAddr *hp;  // Heap pointer. Stores closures. Points to the next value to be allocated.
+static StgAddr *hpLim;  // Marks bottom of heap.
+static StgAddr *hpBack;  // Second heap pointer (for semi-space GC).
+static StgAddr *hpBackLim;  // Bottom of second heap (for semi-space GC).
 
 #endif
