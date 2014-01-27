d3ac065eb44b84af017acc39ca11da5cc4377d40
tcsavage <tcsavage@gmail.com>
2014-01-27 00:37:27 +0000

    Rename STG,hs (typo)

--- src/STG.hs
+++ src/STG.hs
@@ -2,13 +2,15 @@ module STG where
 
 -- The following is the definition of the STG language.
 
+type Var = String
+
 data Program = Program [Binding]
 
 data Binding = Binding Var Lambda
 
-data Lambda = [Var] UpdateFlag [Var] Expr
+data Lambda = Lambda [Var] UpdateFlag [Var] Expr
 
-data UpdateFlag = Updatable | NonUpdatable
+data UpdateFlag = U | N
 
 data Expr = Let [Binding] Expr
           | LetRec [Binding] Expr
@@ -18,6 +20,8 @@ data Expr = Let [Binding] Expr
           | Prim Prim [Atom]
           | Literal Literal
 
+type Constr = Int
+
 data Alts = Algebraic [AAlt] DefAlt
           | Primitive [PAlt] DefAlt
 
@@ -25,7 +29,7 @@ data AAlt = AAlt Constr [Var] Expr
 
 data PAlt = PAlt Literal Expr
 
-data DefAlt = Binding Var Expr
+data DefAlt = DefBinding Var Expr
             | Default Expr
 
 type Literal = Int
@@ -34,3 +38,15 @@ data Prim = PrimAdd | PrimMul
 
 data Atom = AtomVar Var
           | AtomLit Literal
+
+mul :: Binding
+mul = Binding "mul" (Lambda [] N ["x", "y"] (Prim PrimMul [AtomVar "x", AtomVar "y"]))
+
+double :: Binding
+double = Binding "double" (Lambda [] N [] (Ap "mul" [AtomLit 2]))
+
+stgmain :: Binding
+stgmain = Binding "main" (Lambda [] N [] (Let [Binding "x" (Lambda [] N [] (Ap "double" [AtomLit 8]))] (Ap "dumpInt" [AtomVar "x"])))
+
+stgmodule :: Program
+stgmodule = Program [mul, double, stgmain]
