34f8b299a7e72f2769ea306f6e0acf5c37b858b5
tcsavage <tcsavage@gmail.com>
2014-02-01 13:58:48 +0000

    Changes to sample STG programs (mainly adding free variables where required)

--- src/STG.hs
+++ src/STG.hs
@@ -46,19 +46,19 @@ double :: Binding
 double = Binding "double" (Lambda [] N [] (Ap "mul" [AtomLit 2]))
 
 stgmain1 :: Binding
-stgmain1 = Binding "main" (Lambda [] N [] (Let [Binding "x" (Lambda [] N [] (Ap "double" [AtomLit 8]))] (Ap "dumpInt" [AtomVar "x"])))
+stgmain1 = Binding "main" (Lambda [] N [] (Let [Binding "x" (Lambda [] N [] (Ap "double" [AtomLit 2])), Binding "y" (Lambda ["x"] N [] (Ap "double" [AtomVar "x"]))] (Ap "dumpInt" [AtomVar "y"])))
 
 stgmodule1 :: Program
 stgmodule1 = Program [mul, double, stgmain1]
 
 stgmain2 :: Binding
-stgmain2 = Binding "main" (Lambda [] N [] (Let [Binding "x" (Lambda [] N [] (Ap "double" [AtomLit 8]))] (Case (Ap "x" []) (Primitive [PAlt 0 (Ap "dumpInt" [AtomLit 9000])] (DefBinding "z" (Ap "dumpInt" [AtomVar "z"]))))))
+stgmain2 = Binding "main" (Lambda [] N [] (Let [Binding "x" (Lambda [] N [] (Ap "double" [AtomLit 0]))] (Case (Ap "x" []) (Primitive [PAlt 0 (Ap "dumpInt" [AtomLit 9000])] (DefBinding "z" (Ap "dumpInt" [AtomVar "z"]))))))
 
 stgmodule2 :: Program
 stgmodule2 = Program [mul, double, stgmain2]
 
 stgmain3 :: Binding
-stgmain3 = Binding "main" (Lambda [] N [] (Let [Binding "x" (Lambda [] N [] (Ap "double" [AtomLit 8])), Binding "y" (Lambda [] N [] (Constr 0 [AtomVar "x"]))] (Case (Ap "y" []) (Algebraic [AAlt 0 ["foo"] (Ap "dumpInt" [AtomVar "foo"])] (Default (Ap "dumpInt" [AtomLit 1337]))))))
+stgmain3 = Binding "main" (Lambda [] N [] (Let [Binding "x" (Lambda [] N [] (Ap "double" [AtomLit 8])), Binding "y" (Lambda ["x"] N [] (Constr 0 [AtomVar "x"]))] (Case (Ap "y" []) (Algebraic [AAlt 0 ["foo"] (Ap "dumpInt" [AtomVar "foo"])] (Default (Ap "dumpInt" [AtomLit 1337]))))))
 
 stgmodule3 :: Program
 stgmodule3 = Program [mul, double, stgmain3]
